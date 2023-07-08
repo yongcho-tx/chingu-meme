88aede814462f2a0b93d640b361e816362b221fb
Yong Cho <yong-cho@outlook.com>
2023-07-08 17:31:41 -0500

    Navbar Update

--- components/Meme.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Meme = () => {
-  return <div>Display Meme</div>
-}
-
-export default Meme
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -195,31 +195,6 @@ const Navbar = () => {
         </Toolbar>
       </Container>
     </AppBar>
-    // <nav
-    //   style={{
-    //     display: 'flex',
-    //     justifyContent: 'space-between',
-    //     alignItems: 'items-center',
-    //     marginBottom: '4rem',
-    //     paddingTop: '.75rem',
-    //     border: '1px solid white',
-    //   }}
-    // >
-    //   <Link
-    //     href='/'
-    //     style={{
-    //       display: 'flex',
-    //       justifyContent: 'center',
-    //       alignItems: 'center',
-    //       gap: '0.5rem',
-    //     }}
-    //   >
-    //     Logo
-    //   </Link>
-    //   <Link href='/about'>About</Link>
-
-    //   <Link href='/memesdata'>Memes</Link>
-    // </nav>
   )
 }
 
